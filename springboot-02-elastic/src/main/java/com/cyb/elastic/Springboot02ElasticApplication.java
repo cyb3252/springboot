16c8e413b830f57a34f0fc9d6e71a6356df67bfc
cyb <cybthread@163.com>
2018-12-16 11:53:57 +0800

    init elasticsearch project

---
+++ springboot-02-elastic/src/main/java/com/cyb/elastic/Springboot02ElasticApplication.java
@@ -0,0 +1,14 @@
+package com.cyb.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02ElasticApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(Springboot02ElasticApplication.class, args);
+    }
+
+}
+
